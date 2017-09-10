0c3189e32387c0ba263a33e586d6917c61967d11
rupalisurveshi <rupali7veshi@gmail.com>
2017-09-11 00:48:57 +0530

    added name variable

--- Untitled Folder/MainActivity.java
+++ Untitled Folder/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     int quantity = 2 ;
     int price = 10 ;
     int topping = 15 ;
+    int choco = 10 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +27,34 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText(data);
     }
 
-    public void displayOrderSummary(int totalMoney, boolean whippedCream){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        String stringMoney = Integer.toString(totalMoney);
+    public void displayOrderSummary(int totalMoney, boolean whippedCream, boolean chocolate){
         int totalTopping = topping * quantity ;
+        int totalChoco = choco * quantity ;
+        int grandTotal = totalMoney + totalTopping + totalChoco;
+
+        String stringMoney = Integer.toString(totalMoney);
         String stringTopping = Integer.toString(totalTopping);
-        int grandTotal = totalMoney + totalTopping ;
+        String stringChoco = Integer.toString(totalChoco);
         String stringTotal = Integer.toString(grandTotal) ;
-        String message = "Name: Rupali\n" + "Add whipped cream topping: " + whippedCream +"\nQuantity: " + quantity + "\ncoffe charges: " + stringMoney + "\nTopping: " + stringTopping + "\nTotal: $" + stringTotal + "\nThank You!" ;
+
+        EditText nameTextView = (EditText) findViewById(R.id.customer_name_text_view);
+        String customerName = nameTextView.getText().toString();
+
+        String message = "Name: " + customerName + "\nAdd whipped cream topping: " + whippedCream + "\nAdd chocolate : " + chocolate + "\nQuantity: " + quantity + "\ncoffee charges: " + stringMoney + "\nTopping: " + stringTopping + "\nChocolate: " + stringChoco + "\nTotal: $" + stringTotal + "\nThank You!" ;
+
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
 
     public void submitOrder(View view){
-        CheckBox checked = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        boolean isWhippedCream = checked.isChecked();
+        CheckBox whippedCreamChecked = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean isWhippedCream = whippedCreamChecked.isChecked();
+
+        CheckBox chocolateChecked = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean isChocolate = chocolateChecked.isChecked();
+
         int total = quantity * price ;
-        displayOrderSummary(total, isWhippedCream) ;
+        displayOrderSummary(total, isWhippedCream, isChocolate) ;
     }
 
     /**
